77cc9d6aeaf4cb96d67f43c12c1c9a519aa9ae01
ira <ira@debian-ira.uusix.local>
2025-03-13 11:53:59 +0200

    crude login handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,55 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+      window.localStorage.setItem('loggedBlogger', JSON.stringify(user))
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      console.log(`logged in as ${user.username}`)
+    } catch {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => setErrorMessage(null), 5000)
+    }
+  }
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input type='text' value={username} name='username'
+            onChange={({ target }) => setUsername(target.value)}/>
+      </div>
+      <div>
+        password
+          <input type='password' value={password} name='password'
+            onChange={({ target }) => setPassword(target.value)}/>
+      </div>
+      <button type='submit'>login</button>
+    </form>
+  )
+
+  const blogList = () => (
+    <>
+      <h2>logged in as {JSON.parse(window.localStorage.loggedBlogger).username}</h2>
+      <h3>blogs</h3>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+    </>
+  )
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -13,10 +59,8 @@ const App = () => {
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {!user && loginForm()}
+      {user && blogList()}      
     </div>
   )
 }
